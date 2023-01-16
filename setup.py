b101301c991809374d89a0b0331773585533b396
Nicky Pochinkov <work@nicky.pro>
2023-01-16 22:19:19 +0000

    Version 0.1.0

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open("README.rst") as f:
 
 kwargs = {
     "name": "tictactoe-gpt-finetuning",
-    "version": "0.0.4",
-    "description": "Python tic tac toe state generator.",
+    "version": "0.1.0",
+    "description": "Python tic tac toe state generator and GPT fine tuning",
     "author": "Nicky Pochinkov",
     "author_email": "work@nicky.pro",
     "url": "https://github.com/pesvut/tictactoe-gpt-finetuning",
     "license": "MIT",
     "keywords": ["tictactoe", "llm", "language-models"],
-    "install_requires": ["numpy"],
+    "install_requires": ["numpy", "torch", "transformers"],
     "packages": ["tictactoe_gpt_finetuning"],
     "long_description": readme,
 }
